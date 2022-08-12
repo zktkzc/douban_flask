78a01104d5e17512be54f53c315ff1650bc5ca56
tkzc00 <zqzkzy2420586708@qq.com>
2022-08-12 10:43:10 +0800

    douban_flask

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask,render_template
+import sqlite3
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():  # put application's code here
+    return render_template("index.html")
+
+
+@app.route('/index')
+def home():
+    #return render_template("index.html")
+    return index()
+
+
+
+@app.route('/movie')
+def movie():
+    datalist = []
+    con = sqlite3.connect("movie.db")
+    cur = con.cursor()
+    sql = "select * from movie250"
+    data = cur.execute(sql)
+    for item in data:
+        datalist.append(item)
+    cur.close()
+    con.close()
+    return render_template("movie.html",movies = datalist)
+
+
+
+@app.route('/score')
+def score():
+    score = []     #评分
+    num = []       #每个评分所统计到的电影数量
+    con = sqlite3.connect("movie.db")
+    cur = con.cursor()
+    sql = "select score,count(score) from movie250 group by score"
+    data = cur.execute(sql)
+    for item in data:
+        score.append(str(item[0]))
+        num.append(item[1])
+    cur.close()
+    con.close()
+    return render_template("score.html",score = score,num = num)
+
+
+
+@app.route('/team')
+def team():
+    return render_template("team.html")
+
+
+
+@app.route('/word')
+def word():
+    return render_template("word.html")
+
+
+if __name__ == '__main__':
+    app.run()
